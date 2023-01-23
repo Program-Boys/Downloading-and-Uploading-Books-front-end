7ef5d3dce9a74c50e87f2a49de8ea35e58fee900
Guilherme Augusto da Silva <guilhermeshadow91@gmail.com>
2023-01-23 18:33:34 -0300

    feat: setting-token-and-userId-in-login

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,11 +1,27 @@
+import { SubmitFunction } from '../../components/Form';
 import Form from '../../components/Form';
 import Header from '../../components/Header';
+import api from '../../services/api';
 
 const PageLogin = () => {
+  const formSubmit = (data: SubmitFunction) => {
+    api
+      .post('/user/login', data)
+      .then((response) => {
+        const { token, userId } = response.data;
+
+        window.localStorage.setItem('TOKEN', token);
+        window.localStorage.setItem('USERID', userId);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   return (
     <>
       <Header />
-      <Form accountSubmit={() => {}} />
+      <Form accountSubmit={formSubmit} />
     </>
   );
 };
