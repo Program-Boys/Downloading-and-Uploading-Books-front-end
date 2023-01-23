7086e56f41f22d4dcd1306073c15dc51c6937273
Enrique <enrique.barbosasilva@gmail.com>
2023-01-23 16:19:31 -0300

    refactor: code-changed

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,4 +1,4 @@
-import Footer from '../../components/Footer/Index';
+import Footer from '../../components/Footer/index';
 import Form from '../../components/Form';
 import Header from '../../components/Header';
 
