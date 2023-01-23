54c2a38ccb9f6766c5249d960e72d958fed4ce7f
Guilherme Augusto da Silva <guilhermeshadow91@gmail.com>
2023-01-23 18:38:36 -0300

    refactor: changing-Index-to-index

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,14 @@
+import { StyledFooter } from './style';
+
+const Footer = () => {
+  return (
+    <StyledFooter>
+      <p>all rights reserved to </p>
+      <a href="https://github.com/Program-Boys" target="_blank">
+        Program Boys &copy;
+      </a>
+    </StyledFooter>
+  );
+};
+
+export default Footer;
