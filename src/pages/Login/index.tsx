854eb9d29f520b12b8783af58813f513aea7496d
Guilherme Augusto da Silva <guilhermeshadow91@gmail.com>
2023-01-27 19:51:57 -0300

    feat: adding-headers

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -5,20 +5,26 @@ import Header from '../../components/Header';
 import api from '../../services/api';
 import { useContext } from 'react';
 import { UserContext } from '../../context/User/UserContext';
+import { useNavigate } from 'react-router-dom';
 
 const PageLogin = () => {
   const { setIsLogged } = useContext(UserContext);
+  const navigate = useNavigate();
 
   const formSubmit = (data: SubmitFunction) => {
     api
-      .post('/user/login', data)
+      .post('/user/login', data, {
+        headers: { 'Content-Type': 'application/json' },
+      })
       .then((response) => {
+        window.localStorage.clear();
         const { token, userId } = response.data;
 
         window.localStorage.setItem('TOKEN', token);
         window.localStorage.setItem('USERID', userId);
 
         setIsLogged(true);
+        navigate('user-homepage', { replace: true });
       })
       .catch((err) => {
         console.log(err);
