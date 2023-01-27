38b738d8931bba2aa8204ae791e9e45cacdb1fa8
Enrique <enrique.barbosasilva@gmail.com>
2023-01-27 20:34:41 -0300

    refactor: structure-changed

--- src/validations/book.validations.ts
+++ src/validations/book.validations.ts
@@ -1,7 +1,5 @@
 import * as yup from 'yup';
 
 export const bookSchema = yup.object().shape({
-  name: yup.string().required('Nome obrigatório'),
-  gender: yup.string().required('Gênero obrigatório'),
-  file: yup.mixed().required(),
+  file: yup.mixed().required('Arquivo do livro obrigatório'),
 });
